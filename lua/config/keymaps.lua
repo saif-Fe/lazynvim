30395883060f2e5c231dfa91604ade70559d1908
saif-Fe <saif.tafsol@gmail.com>
2023-11-22 07:31:12 +0500

    Css color added

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -28,3 +28,17 @@ end)
 vim.keymap.set("n", "<A-]>", function()
   ui.nav_prev()
 end)
+
+vim.opt.termguicolors = true
+
+local ccc = require("ccc")
+local mapping = ccc.mapping
+
+ccc.setup({
+  -- Your preferred settings
+  -- Example: enable highlighter
+  highlighter = {
+    auto_enable = true,
+    lsp = true,
+  },
+})
